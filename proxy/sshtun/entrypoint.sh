c5f3a0b51b130b95fbc6ae328995668de8d0af1d
Leo <leo.elias@gmail.com>
2022-03-15 17:58:35 -0400

    .

--- proxy/sshtun/entrypoint.sh
+++ proxy/sshtun/entrypoint.sh
@@ -59,6 +59,6 @@ log() {
 
 
 log INFO "Launching ssh tunnel"
-ssh -N -4 -o ConnectTimeout=${__timeout} -D ${__local_socks_bind} -g -L ${__hev_socks_server_local_port}:127.0.0.1:${__hev_socks_server_port} ${__ssh_user}@${__ssh_server} -o "ProxyCommand=/usr/bin/nc -X 5 -x ${__socks_host}:${__socks_port} %h %p"
-
+ssh -N -4 -o ConnectTimeout=${__timeout} -D ${__socks_port} -g -L ${__hev_socks_server_local_port}:127.0.0.1:${__hev_socks_server_port} ${__ssh_user}@${__ssh_server} -o "ProxyCommand=/usr/bin/nc -X 5 -x ${__socks_host}:${__socks_port} %h %p"
+log INFO "Cycling ssh tunnel"
 
